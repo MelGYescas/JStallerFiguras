0f8545ec1d43dc62a12b295410401134b548737c
Mariel Gonzalez <gonzalezyescas@gmail.com>
2021-12-10 17:38:23 -0600

    Funciones de perimetros y areas listas

--- figuras.js
+++ figuras.js
@@ -1,5 +1,52 @@
-const LadoCuadrado = 5;
-console.log(`Los lados del cuadrado miden:  ${LadoCuadrado}`);
+//Código del cuadrado
+console.group("Cuadrados");
+//perimetro
+function perimetroCuadrado(lado) {
+  return lado * 4;
+}
 
-const perimetroCuadrado = LadoCuadrado * 4;
-console.log(`Los lados del cuadrado miden:  ${LadoCuadrado}`);
+//area
+function areaCuadrado(lado) {
+  return lado * lado;
+}
+
+console.groupEnd();
+
+//Código del triangulo
+console.group("Triangulos");
+//Perimetro
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+  }
+//Area
+  function areaTriangulo(base, altura) {
+    return (base * altura) / 2;
+  }
+ 
+  console.groupEnd();
+
+  
+// Código del círculo
+console.group("Círculos");
+
+
+// Diámetro
+function diametroCirculo(radio) {
+  return radio * 2;
+}
+
+// PI
+const PI = Math.PI;
+
+// Circunferencia
+function perimetroCirculo(radio) {
+  const diametro = diametroCirculo(radio);
+  return diametro * PI;
+}
+
+// Área
+function areaCirculo(radio) {
+  return (radio * radio) * PI;
+}
+
+console.groupEnd();
